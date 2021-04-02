bae61886674d7d9f5ada3a40f863a5269025e7a3
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-04-02 13:03:33 +0200

    bug resolve

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,47 +1,47 @@
-using UnityEngine;
+// using UnityEngine;
 
 
-public class PlayerHealth : MonoBehaviour
-{
-        public int maxHealth = 100;
-        public int currentHealth;
+// public class PlayerHealth : MonoBehaviour
+// {
+//         public int maxHealth = 100;
+//         public int currentHealth;
 
-        public HealthBar healthBar;
+//         public HealthBar healthBar;
 
-        public static PlayerHealth instance;
+//         public static PlayerHealth instance;
 
-        void Start()
-        {
-            currentHealth = maxHealth;
-            healthBar.SetMaxHealth(maxHealth);
-        }
+//         void Start()
+//         {
+//             currentHealth = maxHealth;
+//             healthBar.SetMaxHealth(maxHealth);
+//         }
 
 
-    void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.H))
-            {
-                TakeDamage(20);
-            }
-        }
+//     void Update()
+//         {
+//             if (Input.GetKeyDown(KeyCode.H))
+//             {
+//                 TakeDamage(20);
+//             }
+//         }
 
-    void TakeDamage(int damage)
-        {
-            currentHealth -= damage;
-            healthBar.SetHealth(currentHealth);
+//     void TakeDamage(int damage)
+//         {
+//             currentHealth -= damage;
+//             healthBar.SetHealth(currentHealth);
 
-            if(currentHealth <= 0)
-            {
-                Die();
-                return;
-            }
-        }
-    public void Die()
-    {
-        Debug.Log("Le joueur est éliminé");
-        PlayerMovement.instance.enabled = false;
-        PlayerMovement.instance.animator.SetTrigger("Die");
-        PlayerMovement.instance.rb.bodyType = RigidbodyType2D.Kinematic;
-        PlayerMovement.instance.playerCollider.enabled = false;
-    }
-}
+//             if(currentHealth <= 0)
+//             {
+//                 Die();
+//                 return;
+//             }
+//         }
+//     public void Die()
+//     {
+//         Debug.Log("Le joueur est éliminé");
+//         PlayerMovement.instance.enabled = false;
+//         PlayerMovement.instance.animator.SetTrigger("Die");
+//         PlayerMovement.instance.rb.bodyType = RigidbodyType2D.Kinematic;
+//         PlayerMovement.instance.playerCollider.enabled = false;
+//     }
+// }
